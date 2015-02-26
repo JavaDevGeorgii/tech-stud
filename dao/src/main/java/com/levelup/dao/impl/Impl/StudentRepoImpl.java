fa24508367373c5e726d85d04ae7b6745ec965c1
GEO <dghteng@gmail.com>
2015-02-27 00:48:23 +0200

    remake StudentRepoImpl

---
+++ dao/src/main/java/com/levelup/dao/impl/Impl/StudentRepoImpl.java
@@ -0,0 +1,45 @@
+package com.levelup.dao.impl.Impl;
+
+import com.levelup.dao.impl.HibernateSessionProvider;
+import com.levelup.dao.impl.StudentRepo;
+import com.levelup.model.Student;
+import com.sun.tools.javac.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+
+/**
+ * Created by GEO on 26.02.15.
+ */
+public class StudentRepoImpl implements StudentRepo {
+    @Override
+    public void createStudent(Student student) {
+        Session session = HibernateSessionProvider.getSession();
+        session.beginTransaction();
+        session.persist(student);
+        session.beginTransaction().commit();
+        session.close();
+    }
+
+    @Override
+    public List<Student> getStudentByTeacherId(Long id) {
+        Session session = HibernateSessionProvider.getSession();
+        session.beginTransaction();
+        //Query query=session.createQuery("from Student as s where s.teachid = :id");
+        query.setParameter("id", id);
+        List<Student> result = (List<Student>) query.list();
+        session.beginTransaction().commit();
+        session.close();
+        return result;
+    }
+
+    @Override
+    public Student getStudentByName(String lastName) {
+        Session session = HibernateSessionProvider.getSession();
+        session.beginTransaction();
+        Student student= (Student) session.get(Student.class,lastName);
+        session.beginTransaction().commit();
+        session.close();
+        return student;
+    }
+}
